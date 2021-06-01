2f5d3e56083566c9e181f2fda6b1cbf1543f6804
stephsca23 <79933669+stephsca23@users.noreply.github.com>
2021-06-01 11:48:29 -0500

    Ejercicio1

---
+++ Sesion2/Ejercicio1/ejercicio1.js
@@ -0,0 +1,18 @@
+var time = 13;
+var saludo;
+
+if (time < 12) {
+    saludo = "buenos dias";
+
+}
+
+if (time < 20) {
+    saludo = "buenas tardes";
+
+}
+
+if (time >= 20) {
+    saludo = "buenas noches";
+}
+
+
